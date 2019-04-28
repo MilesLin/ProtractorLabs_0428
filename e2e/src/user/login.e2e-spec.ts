2ca28896b896c54a7a8275d5425dbde6c895a4eb
Miles <milesasp@gmail.com>
2019-04-28 11:41:35 +0800

    實戰演練：完成登入功能的 E2E 測試

--- e2e/src/user/login.e2e-spec.ts
+++ e2e/src/user/login.e2e-spec.ts
@@ -1,3 +1,22 @@
-describe('the user try to login', () => {
+import { element, by, browser } from "protractor";
 
+fdescribe('the user try to login', () => {
+    beforeEach(() => {
+        browser.get('/user/login');
+    });
+    it('should login to event page', () => {
+        element(by.id('userName')).sendKeys('John');
+        element(by.id('password')).sendKeys('123456');
+        element(by.buttonText('登入')).click();
+        const url = browser.getCurrentUrl();
+        expect(url).toBe(browser.baseUrl + 'events');
+    });
+
+    it('should show login failed when typing wrong password', () => {
+        element(by.id('userName')).sendKeys('John');
+        element(by.id('password')).sendKeys('abc');
+        element(by.buttonText('登入')).click();
+        const isAlertPresent = element(by.className('alert-danger')).isPresent();
+        expect(isAlertPresent).toBe(true, '錯誤密碼案例驗證失敗');
+    });
 });
