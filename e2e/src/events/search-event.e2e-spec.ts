0e9adf7476339f0b2060377e4390a98fcb8411dc
Miles <milesasp@gmail.com>
2019-04-28 14:39:58 +0800

    實戰演練：練習複雜 DOM 定位運用

--- e2e/src/events/search-event.e2e-spec.ts
+++ e2e/src/events/search-event.e2e-spec.ts
@@ -1,4 +1,44 @@
-describe('search a event', () => {
+import { browser, by, element, ElementFinder } from 'protractor';
 
+fdescribe('search a event', () => {
+    let searchTerm: ElementFinder;
+    let searchBtn: ElementFinder;
+  
+    beforeAll(async () => {
+      searchTerm = element(by.name('searchTerm'));
+      searchBtn = element(by.buttonText('搜尋'));
+    });
+  
+    it('should navigate to event page', async () => {
+      await browser.get('/events');
+      expect(await browser.getCurrentUrl()).toBe(browser.baseUrl + 'events');
+    });
+  
+    it('should type in angular', async () => {
+      await searchTerm.sendKeys('Angular');
+      expect(await searchTerm.getAttribute('value')).toBe('Angular');
+    });
+  
+    it('should click search and pop up a modal', async () => {
+      await searchBtn.click();
+      const isDisplayed = await element(by.className('modal-dialog')).isDisplayed();
+      expect(isDisplayed).toBe(true);
+    });
+  
+    it('should have three result', async () => {
+  
+        const resultCount = await element.all(by.className('list-group-item')).count();
+    
+        expect(resultCount).toBe(3);
+    });
+
+    it('should click the second search result', async () => {
+  
+      await element(by.className('list-group')).element(by.cssContainingText('a', '實戰開發')).click();
+  
+      // 驗證第一個 h2 是 「ANGULAR 7 開發實戰：新手入門篇」
+      const text = await element(by.tagName('h2')).getText();
+      expect(text).toBe('ANGULAR 7 開發實戰：新手入門篇');
+    });
 });
 
--- e2e/src/labs/questionnaire.e2e-spec.ts
+++ e2e/src/labs/questionnaire.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
-fdescribe('the user submit a questionnaire', () => {
+describe('the user submit a questionnaire', () => {
     it('should type information', async () => {
         await browser.get('labs/questionnaire');
         await element(by.name('username')).sendKeys('John');
